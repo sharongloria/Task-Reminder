aa8bea0908495c2929af4dcc23eb9d5470386a64
sharongloria <sharongloria96@gmail.com>
2024-11-18 23:47:14 +0300

    Main File

--- main.py
+++ main.py
@@ -21,7 +21,41 @@ def view_reminders():
     else:
         print("\nNo reminders set.")
 
+def check_reminders():
+    while True:
+        now = datetime.now()
+        to_remove = []
+        for reminder_time, task in reminders.items():
+            if reminder_time <= now:
+                print(f'\n Reminder: {task} is due now!')
+                to_remove.append(reminder_time)
+        for time_to_remove in to_remove:
+            del reminders[time_to_remove]
+        time.sleep(60)
 
+def main():
+    print(" Welcome to your Reminder Assistant!")
+    while True:
+        print("\nChoose aan option:")
+        print("1: Add a new Reminder")
+        print("2: View all Reminders")
+        print("3: Start checking for Reminders")
+        print("4: Exit")
 
-add_reminder()
-view_reminders()
+        choice = input("Enter your choice (1/2/3/4)")
+
+        if choice == '1':
+            add_reminder()
+        elif choice == '2':
+            view_reminders()
+        elif choice == '3':
+            print("Checking for reminders...")
+            check_reminders()
+        elif choice == '4':
+            print("Goodbye")
+            break
+        else:
+            print("Invalid choice. Enter 1/2/3/4")
+
+if __name__ == "__main__":
+    main()
